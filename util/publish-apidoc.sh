e16c1dd6edc868ff5fae992ed4bb940291f739a7
cuponthetop <cuponthetop@gmail.com>
2016-02-03 23:49:56 +0900

    doc file update test

--- util/publish-apidoc.sh
+++ util/publish-apidoc.sh
@@ -2,10 +2,10 @@ BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 apidoc="./node_modules/.bin/apidoc"
 
-out=$($apidoc -i ./lib/route/v1/ -o apidoc/ 2>&1)
+out=$($apidoc -i ./lib/route/v1/ -o ../apidocout/ 2>&1)
 status=$?
 if [ "$status" != "0" ]; then
-    rm -rf apidoc/
+    rm -rf apidocout/
     git checkout $BRANCH
     echo "$out"
     exit $status
@@ -13,6 +13,10 @@ fi
 
 # change to gh-pages branch
 git checkout gh-pages
+
+rm -rf apidoc/
+
+cp -r apidocout apidoc
 
 git add apidoc
 
@@ -23,4 +27,4 @@ git push origin gh-pages
 # change to original branch
 git checkout $BRANCH
 
-rm -rf apidoc/
+rm -rf apidocout/
