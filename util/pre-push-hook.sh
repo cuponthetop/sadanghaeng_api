95a0108146fc9b56e19468bbf550228e60a35f5d
cuponthetop <cuponthetop@gmail.com>
2016-02-03 22:53:56 +0900

    apidoc test

--- util/pre-push-hook.sh
+++ util/pre-push-hook.sh
@@ -10,6 +10,28 @@ PUSH_COMMAND=`ps -ocommand= -p $PPID`
 PROTECTED_BRANCHES="^(master)"
 FORCE_PUSH="force|delete|-f"
 
+# change to gh-pages branch
+$(git checkout gh-pages)
+
+apidoc="./node_modules/.bin/apidoc"
+
+out=$($apidoc -i lib/route/v1/ -o apidoc/ 2>&1)
+status=$?
+if [ "$status" != "0" ]; then
+    $(git checkout $BRANCH)
+    echo "$out"
+    exit $status
+fi
+
+$(git add apidoc)
+
+$(git commit -m "api doc update")
+
+$(git push origin gh-pages)
+
+# change to original branch
+$(git checkout $BRANCH)
+
 if [[ "$BRANCH" =~ $PROTECTED_BRANCHES && "$PUSH_COMMAND" =~ $FORCE_PUSH ]]; then
   echo "Prevented force-push to protected branch \"$BRANCH\" by pre-push hook"
   exit 1
