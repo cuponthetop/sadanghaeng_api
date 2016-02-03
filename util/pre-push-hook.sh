99b10b5d9682db650eab0f4b0a60ae6f4a768b2e
cuponthetop <cuponthetop@gmail.com>
2016-02-03 23:00:38 +0900

    asdf

--- util/pre-push-hook.sh
+++ util/pre-push-hook.sh
@@ -31,6 +31,7 @@ $(git push origin gh-pages)
 
 # change to original branch
 $(git checkout $BRANCH)
+echo "test"
 
 if [[ "$BRANCH" =~ $PROTECTED_BRANCHES && "$PUSH_COMMAND" =~ $FORCE_PUSH ]]; then
   echo "Prevented force-push to protected branch \"$BRANCH\" by pre-push hook"
