3f755b7ec708808dded6f2565d19367449ba0d51
cuponthetop <cuponthetop@gmail.com>
2016-02-05 12:47:40 +0900

    lambda expression

--- test/unit/university/university-email-check-specs.js
+++ test/unit/university/university-email-check-specs.js
@@ -5,30 +5,28 @@ process.env.NODE_ENV = 'test';
 var chai = require('../../helper/setup-chai');
 var UnivCtrl = require('../../../lib/controller/university');
 
-
-
-describe('UniversityController', function () {
-  describe('#getUniversityFromEmail', function () {
+describe('UniversityController', () => {
+  describe('#getUniversityFromEmail', () => {
 
     var config = require('../../../config/config');
     var mongoose = require('mongoose');
     var wasConnected = mongoose.connection.readyState;
 
-    // before(() => {
-    //   if (wasConnected === 0) {
+    before(() => {
+      if (wasConnected === 0) {
 
-    //     // 몽고 db 연결
-    //     var dbUri = config.db.uri + config.db.dbName;
-    //     var dbOptions = { username: config.db.username, password: config.db.password };
-    //     mongoose.connect(dbUri, dbOptions);
-    //   }
-    // });
+        // 몽고 db 연결
+        var dbUri = config.db.uri + config.db.dbName;
+        var dbOptions = { username: config.db.username, password: config.db.password };
+        mongoose.connect(dbUri, dbOptions);
+      }
+    });
 
-    // after(() => {
-    //   if (wasConnected === 0) {
-    //     mongoose.disconnect();
-    //   }
-    // });
+    after(() => {
+      if (wasConnected === 0) {
+        mongoose.disconnect();
+      }
+    });
 
     it('should reject invalid email address', (done) => {
       UnivCtrl.getUniversityFromEmail('shouldreject')
@@ -42,19 +40,19 @@ describe('UniversityController', function () {
         .notify(done);
     });
 
-    it('should reject email domain where no university accepts', function (done) {
+    it('should reject email domain where no university accepts', (done) => {
       UnivCtrl.getUniversityFromEmail('test@test3.com')
         .should.be.rejectedWith('none of universities we support have ' + 'test3.com')
         .notify(done);
     });
 
-    it('should reject email domain where more than 2 universities accepts', function (done) {
+    it('should reject email domain where more than 2 universities accepts', (done) => {
       UnivCtrl.getUniversityFromEmail('test@test2.com')
         .should.be.rejectedWith('many of universities we support have ' + 'test2.com' + ' as their email!!')
         .notify(done);
     });
 
-    it('should accept valid email domain', function (done) {
+    it('should accept valid email domain', (done) => {
       UnivCtrl.getUniversityFromEmail('test@test.com')
         .should.eventually.equal('56ac6f7b9b0d0b0457673daf')
         .notify(done);
--- test/unit/user/user-manage-specs.js
+++ test/unit/user/user-manage-specs.js
@@ -5,15 +5,15 @@ process.env.NODE_ENV = 'test';
 var request = require('supertest');
 var chai = require('../../helper/setup-chai');
 var expect = chai.expect;
-var app = require('../../../lib/server/test-server');
+var app = request('http://localhost:3001');
 
-describe('UserController', function () {
-  describe('#getUser', function () {
-    it('respond with json', function (done) {
-      request(app)
+describe('UserController', () => {
+  describe('#getUser', () => {
+    it('respond with json', (done) => {
+      app
         .get('/api/v1/users/' + '11bc6f7b9b0d0b0457673daf')
         .set('Accept', 'application/json')
-        .end(function (err, res) {
+        .end( (err, res) => {
           console.log(JSON.stringify(res));
           expect(res.body.value.email).to.equal('test@test.com');
           done();
