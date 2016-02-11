71d5d4c5d08582774bf1c5e4e526d3aa34f07462
cuponthetop <cuponthetop@gmail.com>
2016-02-12 03:05:38 +0900

    status_all added

--- util/status_api_creator.js
+++ util/status_api_creator.js
@@ -7,9 +7,17 @@ var status = require('../lib/server/status')
   ;
 
 var createStatusDoc = function () {
-  var location = path.resolve(__dirname, '../lib/route/status_api.js');
+  var location = path.resolve(__dirname, '../lib/route');
 
-  var filestream = fs.createWriteStream(location, {
+  var filestream = fs.createWriteStream(path.resolve(location, 'status_api.js'), {
+    flags: 'w',
+    defaultEncoding: 'utf8',
+    fd: null,
+    mode: 0o666,
+    autoClose: true
+  });
+
+  var filestream2 = fs.createWriteStream(path.resolve(location, 'status_all.js'), {
     flags: 'w',
     defaultEncoding: 'utf8',
     fd: null,
@@ -18,6 +26,9 @@ var createStatusDoc = function () {
   });
 
   filestream.write('"use strict";\n\n');
+  filestream2.write('"use strict";\n\n');
+
+  filestream2.write('/**\n * @api {get} / Status Codes\n * @apiName StatusCodes\n * @apiGroup StatusCodes\n *\n');
 
   _.mapObject(status.codes, (val, key) => {
     if (key !== 'Success') {
@@ -25,9 +36,12 @@ var createStatusDoc = function () {
       filestream.write(' * @apiDefine ' + key +'\n');
       filestream.write(' * @apiError ' + key + ' (' + val.code + ') ' + val.summary + '\n');
       filestream.write(' */\n\n');
+
+      filestream2.write(' * @apiUse ' + key + '\n');
     }
   });
 
+  filestream2.end(' */\n');
   filestream.end('');
   filestream.on('finish', () => {
     console.error('all writes are now complete.');
