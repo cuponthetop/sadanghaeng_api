2c76bc30a7300f4ad704131d8dfd49592d949f9f
cuponthetop <cuponthetop@gmail.com>
2016-02-12 00:23:00 +0900

    email domain check + university manage test code + status

--- test/api/university/univ-manage-specs.js
+++ test/api/university/univ-manage-specs.js
@@ -15,6 +15,8 @@ var request = require('supertest-session')('http://localhost:3001')
 
 describe('University API Manage', () => {
 
+  var newUnivId = '';
+
   before((done) => {
     // 몽고 db 연결
     var dbUri = config.db.uri + config.db.dbName;
@@ -95,16 +97,211 @@ describe('University API Manage', () => {
   });
 
   describe('#createUniversity', () => {
+    it('should not allow anonymous users to create university', (done) => {
+      request
+        .post('/api/v1/universities')
+        .send({
+          name: 'testUniv4',
+          displayName: 'testUniv4',
+          emailDomainLists: ['test4.com']
+        })
+        .end((err, res) => {
+          res.body.status.should.be.equal(status.codes.UserAuthRequired.code);
+          res.body.should.not.have.property('value');
+          done();
+        });
+    });
 
+    it('should not allow normal users to create university', (done) => {
+      login.login(request, 'test2@test.com', 'test').then(() => {
+        request
+          .post('/api/v1/universities')
+          .send({
+            name: 'testUniv4',
+            displayName: 'testUniv4',
+            emailDomainLists: ['test4.com']
+          })
+          .end((err, res) => {
+            res.body.status.should.be.equal(status.codes.UserPermissionNotAllowed.code);
+            res.body.should.not.have.property('value');
+            login.logout(request).then(() => {
+              done();
+            });
+          });
+      });
+    });
+
+    it('should not allow admin users to create university with invalid email domain', (done) => {
+      login.login(request, 'admin@test.com', 'test').then(() => {
+        request
+          .post('/api/v1/universities')
+          .send({
+            name: 'testUniv4',
+            displayName: 'testUniv4',
+            emailDomainLists: ['test4']
+          })
+          .end((err, res) => {
+            res.body.status.should.be.equal(0);
+            newUnivId = res.body.value;
+            login.logout(request).then(() => {
+              done();
+            });
+          });
+      });
+    });
+
+    it('should not allow admin users to create university with existing university name', (done) => {
+      login.login(request, 'admin@test.com', 'test').then(() => {
+        request
+          .post('/api/v1/universities')
+          .send({
+            name: 'testUniv3',
+            displayName: 'testUniv4',
+            emailDomainLists: ['test4.com']
+          })
+          .end((err, res) => {
+            res.body.status.should.be.equal(status.codes.UnivAlreadyExisting.code);
+            newUnivId = res.body.value;
+            login.logout(request).then(() => {
+              done();
+            });
+          });
+      });
+    });
+
+    it('should allow admin users to create university', (done) => {
+      login.login(request, 'admin@test.com', 'test').then(() => {
+        request
+          .post('/api/v1/universities')
+          .send({
+            name: 'testUniv4',
+            displayName: 'testUniv4',
+            emailDomainLists: ['test4.com']
+          })
+          .end((err, res) => {
+            res.body.status.should.be.equal(0);
+            newUnivId = res.body.value;
+            login.logout(request).then(() => {
+              done();
+            });
+          });
+      });
+    });
   });
-  
-  describe('#destroyUniversity', () => {
-
-  });
-
 
   describe('#updateUniversity', () => {
+    it('should not allow anonymous users to update university', (done) => {
+      request
+        .put('/api/v1/universities/' + newUnivId)
+        .send({
+          name: 'shouldnot accept this',
+          displayName: 'should not accept this',
+          emailDomainLists: ['should not accept this']
+        })
+        .end((err, res) => {
+          res.body.status.should.be.equal(status.codes.UserAuthRequired.code);
+          res.body.should.not.have.property('value');
+          done();
+        });
+    });
 
+    it('should not allow normal users to update university', (done) => {
+      login.login(request, 'test@test.com', 'test').then(() => {
+        request
+          .put('/api/v1/universities/' + newUnivId)
+          .send({
+            name: 'shouldnotaccept',
+            displayName: 'shouldnotaccept',
+            emailDomainLists: ['shouldnotaccepts']
+          })
+          .end((err, res) => {
+            res.body.status.should.be.equal(status.codes.UserPermissionNotAllowed.code);
+            res.body.should.not.have.property('value');
+            login.logout(request).then(() => {
+              done();
+            });
+          });
+      });
+    });
+
+    it('should not allow admin users to update university with invalid email domain', (done) => {
+      login.login(request, 'admin@test.com', 'test').then(() => {
+        request
+          .put('/api/v1/universities/' + newUnivId)
+          .send({
+            name: 'changeUniv4',
+            displayName: 'changeUniv4',
+            emailDomainLists: ['test4']
+          })
+          .end((err, res) => {
+            res.body.status.should.be.equal(status.codes.InvalidEmailDomain.code);
+            res.body.should.not.have.property('value');
+            login.logout(request).then(() => {
+              done();
+            });
+          });
+      });
+    });
+
+    it('should allow admin users to update university', (done) => {
+      login.login(request, 'admin@test.com', 'test').then(() => {
+        request
+          .put('/api/v1/universities/' + newUnivId)
+          .send({
+            name: 'changeUniv4',
+            displayName: 'changeUniv4',
+            emailDomainLists: ['test4.com']
+          })
+          .end((err, res) => {
+            res.body.status.should.be.equal(0);
+            newUnivId = res.body.value;
+            login.logout(request).then(() => {
+              done();
+            });
+          });
+      });
+    });
   });
+
+  describe('#destroyUniversity', () => {
+    it('should not allow anonymous users to destroy university', (done) => {
+      request
+        .del('/api/v1/universities/' + newUnivId)
+        .end((err, res) => {
+          res.body.status.should.be.equal(status.codes.UserAuthRequired.code);
+          res.body.should.not.have.property('value');
+          done();
+        });
+    });
+
+    it('should not allow normal users to destroy university', (done) => {
+      login.login(request, 'test@test.com', 'test').then(() => {
+        request
+          .del('/api/v1/universities/' + newUnivId)
+          .end((err, res) => {
+            res.body.status.should.be.equal(status.codes.UserPermissionNotAllowed.code);
+            res.body.should.not.have.property('value');
+            login.logout(request).then(() => {
+              done();
+            });
+          });
+      });
+    });
+
+    it('should allow admin users to destroy university', (done) => {
+      login.login(request, 'admin@test.com', 'test').then(() => {
+        request
+          .del('/api/v1/universities/' + newUnivId)
+          .end((err, res) => {
+            res.body.status.should.be.equal(0);
+            res.body.should.not.have.property('value');
+            login.logout(request).then(() => {
+              done();
+            });
+          });
+      });
+    });
+  });
+
 });
 
