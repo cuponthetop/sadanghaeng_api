b58f29e3d3a91de11a9a8f69b20f394fc0f17cd0
HwasooSung <hwasu92@naver.com>
2016-02-11 21:07:43 +0900

    change response statement, add status

--- test/unit/post/post-get-comments-on-post-spec.js
+++ test/unit/post/post-get-comments-on-post-spec.js
@@ -1,38 +1,38 @@
-'use-strict';
+// 'use-strict';
 
-process.env.NODE_ENV = 'test';
+// process.env.NODE_ENV = 'test';
 
-var chai = require('../../helper/setup-chai');
-var PostCtrl = require('../../../lib/controller/post');
+// var chai = require('../../helper/setup-chai');
+// var PostCtrl = require('../../../lib/controller/post');
 
-describe('PostController', () => {
-  describe('#getCommtsOnPost', () => {
+// describe('PostController', () => {
+//   describe('#getCommtsOnPost', () => {
 
-    var config = require('../../../config/config');
-    var mongoose = require('mongoose');
+//     var config = require('../../../config/config');
+//     var mongoose = require('mongoose');
 
-    before(() => {
-      var dbUri = config.db.uri + config.db.dbName;
-      var dbOptions = { username: config.db.user.name, password: config.db.password };
-      mongoose.connect(dbUri, dbOptions); // what is 'dbOptions' for?
-    });
-    after(() => {
-      mongoose.disconnect();
-    });
+//     before(() => {
+//       var dbUri = config.db.uri + config.db.dbName;
+//       var dbOptions = { username: config.db.username, password: config.db.password };
+//       mongoose.connect(dbUri, dbOptions); // what is 'dbOptions' for?
+//     });
+//     after(() => {
+//       mongoose.disconnect();
+//     });
 
-    it('should reject an invalid pid', (done) => {
-      PostCtrl.getCommentsOnPost(/* req, res */)
-        .should.be.rejectedWith('not valid post ID')
-        .notify(done);
-    });
+//     it('should reject an invalid pid', (done) => {
+//       PostCtrl.getCommentsOnPost(/* req, res */)
+//         .should.be.rejectedWith('not valid post ID')
+//         .notify(done);
+//     });
 
-    it('should return right comments', (done) => {
-      PostCtrl.getCommentsOnPost(/* req, res */)
-        .should.equal(/* comments */)
-        .notify(done);
-    });
+//     it('should return right comments', (done) => {
+//       PostCtrl.getCommentsOnPost(/* req, res */)
+//         .should.equal(/* comments */)
+//         .notify(done);
+//     });
 
 
 
-  });
-});
+//   });
+// });
