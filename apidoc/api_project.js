dd54e3409aeb97842b31d37ca81f68bcd29e79d6
cuponthetop <cuponthetop@gmail.com>
2016-02-12 02:56:15 +0900

    api doc update

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-02-11T17:08:38.529Z",
+    "time": "2016-02-11T17:56:15.698Z",
     "url": "http://apidocjs.com",
     "version": "0.14.0"
   }
