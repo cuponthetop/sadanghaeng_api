94c083d442a7d325cccaef2fb764dffa15cf03e7
cuponthetop <cuponthetop@gmail.com>
2016-02-13 16:46:45 +0900

    setup-supertest to promisify supertest-session

--- test/api/university/univ-search-specs.js
+++ test/api/university/univ-search-specs.js
@@ -2,16 +2,18 @@
 
 process.env.NODE_ENV = 'test';
 
-// var request = require('supertest-session')('http://localhost:3001')
+// var request = require('../../helper/setup-supertest')('http://localhost:3001')
 //   , chai = require('../../helper/setup-chai')
 //   , status = require('../../../lib/server/status')
-//   , login = require('../../helper/login')
+//   , login = require('../../helper/login')(request)
+//   , logout = require('../../helper/logout')(request)
 //   ;
 
 
 describe('University API Search', () => {
 
   describe('#searchPosts', () => {
+
   });
 
 });
