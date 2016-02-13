4da4beeadb43fb6adddfda3e4426796caf3911e6
cuponthetop <cuponthetop@gmail.com>
2016-02-14 03:26:35 +0900

    filter type and sort type added

--- config/test/post.js
+++ config/test/post.js
@@ -4,5 +4,7 @@ module.exports = {
   maxAge: 365,
   maxPage: 10000,
   maxPerPage: 30,
-  searchFields: ['title', 'text']
+  searchFields: ['title', 'text'],
+  filterTypes: ['hot', 'new'],
+  sortTypes: ['asc', 'desc']
 };
