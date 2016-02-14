85dacf72def76cac4218822ff0267cdc673fbff8
cuponthetop <cuponthetop@gmail.com>
2016-02-14 16:57:26 +0900

    permitStudent fix

--- test/api/university/univ-posts-specs.js
+++ test/api/university/univ-posts-specs.js
@@ -5,8 +5,8 @@ process.env.NODE_ENV = 'test';
 var request = require('../../helper/setup-supertest')('http://localhost:3001')
   , chai = require('../../helper/setup-chai')
   , status = require('../../../lib/server/status')
-  , login = require('../../helper/login')
-  , logout = require('../../helper/logout')
+  , login = require('../../helper/login')(request)
+  , logout = require('../../helper/logout')(request)
   ;
 
 
