e9c0a68e4ed74efddbf5fed0f629e38886b48e6c
luffy-papi <zlee87@snu.ac.kr>
2016-02-16 13:57:59 +0900

    addcomment tests, 2 pass 1 fail, all 3 close api server

--- test/api/comment/comment-add-specs.js
+++ test/api/comment/comment-add-specs.js
@@ -7,26 +7,62 @@ var request = require('../../helper/setup-supertest')('http://localhost:3001')
   , status = require('../../../lib/server/status')
   , login = require('../../helper/login')(request)
   , logout = require('../../helper/logout')(request)
+  , mongoInit = require('../../init/mongo-init')
+  , commentInit = require('../../init/comments-init')
   ;
 
 describe('Add Comment API', () => {
 
+	before((done) => {
+		mongoInit.connect().then(commentInit).catch(console.log).fin(done);
+	});		
+
+	after((done) => {
+		commentInit().then(mongoInit.disconnect).catch(console.log).fin(done);
+	});
+
 	describe('#postComment', () => {
 		it('should not allow anonymous users to post new comment', (done) => {
-      done();
+ 			request
+				.post('/api/v1/comments/')
+				.send({text: 'you suck'})
+				.end((err, res) => {
+					res.body.status.should.be.equal(status.codes.UserAuthRequired.code);
+					res.body.value.should.have.property('message');
+					done();
+				});
 		});
 
-		it('should allow logged-in users to post new comment', (done) => {
-      done();
-		});
+		// it('should not be an empty post', (done) => {
+		// 	login('test@test.com', 'test').then(() => {
+		// 		request
+		// 		.post('/api/v1/comments/')
+		// 		.send({text: ''})
+		// 		.end((err, res) => {
+		// 			res.body.status.shoud.be.equal(status.codes.EmptyComment.code);
+  //           		res.body.value.should.have.property('message');
+		// 		});
+		// 	logout().then(done);
+		// 	});
+		// });
 
-		it('should have a title', (done) => {
-      done();
-		});
-
-		it('should not be an empty post', (done) => {
-      done();
-		});
+		// it('should allow logged-in users to post new comment', (done) => {
+		// 	login('test@test.com', 'test').then(() => {
+		// 		request
+		// 		.post('/api/v1/comments/')
+		// 		.send({text: 'yay im logged in'})
+		// 		.end((err, res) => {
+		// 			res.body.status.shoud.be.equal(0);
+  //           		res.body.value.should.exist();
+  //           		request
+  //           			.get('/api/v1/comments/')
+  //           			.end((err, res) => {
+  //           				expect(res.body.value).to.include.members({ text: 'yay im logged in' });
+  //           			});
+		// 		});
+		// 	logout().then(done);
+		// 	});
+		// });
 	});
 
 });
