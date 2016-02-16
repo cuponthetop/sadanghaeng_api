25d42f76e5a2e8c4b4b313214d5d2d1699c15c86
luffy-papi <zlee87@snu.ac.kr>
2016-02-16 13:26:02 +0900

    vote comment tests added, anoyn test fails

--- test/api/comment/comment-vote-specs.js
+++ test/api/comment/comment-vote-specs.js
@@ -25,39 +25,68 @@ describe('Vote comment API', () => {
 	describe('#voteComment', () => {
 		var cid = '77ac6f7b9b0d0b0457673daf';
 
-		it('should get right post to vote on + allow logged-in users to vote', (done) => {
-			login('test@test.com', 'test')
-				.then(() => {
-					return request
-						.post('/api/v1/comments/' + cid + '/votes')
-						.expect(200)
-						.toPromise();
-				})
-				.then((res) => {
-          			console.log(JSON.stringify(res));
-					res.body.status.should.be.equal(0);
-					res.body.value.should.exist();
-				})
-				.then(logout)
-				.then(done)
-				.catch(done)
-				.done();
-		});
-
 		it('should not allow anonymous users to vote', (done) => {
 			request
 				.post('/api/v1/comments/' + cid + '/votes')
+				.send({type: 'malicious text'})
 				.end((err, res) => {
-					console.log('res: ');
-					console.log(JSON.stringify(res));
+					// console.log('res: ');
+					// console.log(JSON.stringify(res));
+					// console.log('\n');
+					// console.log('err: ');
+					// console.log(err);
 					res.body.status.should.be.equal(status.codes.UserAuthRequired.code);
 					res.body.value.should.have.property('message');
 					done();
 				});
 		});
 
+		it('vote should not be empty', (done) => {
+			login('test@test.com', 'test').then(() => {
+				request
+				.post('/api/v1/comments/' + cid + '/votes')
+				.send({type: ''})
+				.end((err, res) => {
+					res.body.status.shoud.be.equal(status.codes.EmptyVote.code);
+            		res.body.value.should.have.property('message');
+				});
+			logout().then(done);
+			});
+		});
+
+		it('vote should be only up- or down-vote', (done) => {
+			login('test@test.com', 'test').then(() => {
+				request
+				.post('/api/v1/comments/' + cid + '/votes')
+				.send({type: 'lol'})
+				.end((err, res) => {
+					res.body.status.shoud.be.equal(status.codes.WrongVote.code);
+            		res.body.value.should.have.property('message');
+				});
+			logout().then(done);
+			});
+		})
+
+		it('should get right post to vote on and allow logged-in users to vote', (done) => {
+			login('test@test.com', 'test').then(() => {
+				request
+				.post('/api/v1/comments/' + cid + '/votes')
+				.send({type: 'upvote'})
+				.end((err, res) => {
+					res.body.status.shoud.be.equal(0);
+            		res.body.value.should.exist();
+            		request
+            			.get('/api/v1/comments/' + cid)
+            			.end((err, res) => {
+            				expect(res.body.value).to.include.members({ votes: ['upvote'] });
+            			});
+				});
+			logout().then(done);
+			});
+		});
+
 		it('should only allow user to vote once', (done) => {
-      done();
+			done();
 		});
 	});
 
